68d85416156d4e1a7a3b79a4b587dcbc24c5b368
choueric <zhssmail@gmail.com>
2015-08-13 22:47:11 +0200

    add git global config for user.name and email.

--- setup.sh
+++ setup.sh
@@ -13,3 +13,6 @@ if -e ~/.git-completion.bash; then
 	mv ~/.git-completion.bash ~/.git-completion.bash
 fi
 ln -s `pwd`/git-completion.bash ~/.git-completion.bash
+
+git config --global user.name choueric
+git config --global user.email zhssmail@gmail.com
