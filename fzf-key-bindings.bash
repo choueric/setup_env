08839c667d00ac60634cb804c19e00baef867ea8
zhs <haishan.zhou@magewell.com>
2018-05-24 14:56:27 +0800

    add fzf key bindings config file

---
+++ fzf-key-bindings.bash
@@ -0,0 +1,23 @@
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+gitst() {
+  # "Nothing to see here, move along"
+  is_in_git_repo || return
+
+  local cmd="${FZF_CTRL_T_COMMAND:-"command git status -s"}"
+
+  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" fzf -m "$@" | while read -r item; do
+    printf '%q ' "$item" | cut -d " " -f 2
+  done
+  echo
+}
+
+if [[ ! -o vi ]]; then
+  # CTRL-G - list files of git status
+  bind '"\C-g": " \C-u \C-a\C-k$(gitst)\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\er \C-h"'
+else
+  # CTRL-G - list files of git status
+  bind '"\C-g": "\C-x\C-a$a \C-x\C-addi$(gitst)\C-x\C-e\C-x\C-a0Px$a \C-x\C-r\C-x\C-axa "'
+fi
