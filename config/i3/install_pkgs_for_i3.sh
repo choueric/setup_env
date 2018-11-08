43dc650aea3b7b171b3aa55f61132934b8117d0b
Haishan Zhou <zhssmail@gmail.com>
2018-11-08 13:35:08 +1300

    i3: install San Francisco Font

--- config/i3/install_pkgs_for_i3.sh
+++ config/i3/install_pkgs_for_i3.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 DISTR=`lsb_release  -a 2> /dev/null | grep Distributor | cut -f 2`
+SFDFontURL="https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip"
+SFDZip=SanFrancisoFont.zip
 
 # for Ubuntu
 
 if [ "$DISTR" = "Ubuntu" ]; then
     echo "Install for Ubuntu ..."
+    wget $SFDFontURL -O $SFDZip
+    unzip $SFDZip
+    mkdir -p ~/.fonts
+    cp -v ./YosemiteSanFranciscoFont-master/*.ttf ~/.fonts
     sudo apt install feh arandr rofi fonts-font-awesome compton i3blocks
 else
     echo "Not support $DISTR"
