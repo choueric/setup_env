ee20bd6fdf83827a4119fcb32e644131242d0654
Haishan Zhou <zhssmail@gmail.com>
2019-01-07 14:54:36 +1300

    fzf: fix problem picking git status item

--- fzf-key-bindings.bash
+++ fzf-key-bindings.bash
@@ -9,7 +9,7 @@ gitst() {
   local cmd="${FZF_CTRL_T_COMMAND:-"command git status -s"}"
 
   eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" fzf -m "$@" | while read -r item; do
-    printf '%q ' "$item" | cut -d " " -f 2
+    echo "$item" | awk '{print $2}'
   done
   echo
 }
