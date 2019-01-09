897ef19cea33882efab2a9d9740c8eb341d27e3b
choueric <zhssmail@gmail.com>
2019-01-09 23:43:11 +1300

    i3: add volnoti support for brightness

--- config/i3/backlight_brightness.sh
+++ config/i3/backlight_brightness.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+ICON=/usr/share/pixmaps/volnoti/display-brightness-symbolic.svg
+
 VAL=`cat /sys/class/backlight/intel_backlight/actual_brightness`
 MAX=`cat /sys/class/backlight/intel_backlight/max_brightness`
 
 let "val = $VAL * 100 / $MAX"
 echo $val%
+
+volnoti-show -s $ICON $val
