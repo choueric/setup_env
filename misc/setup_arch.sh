a851f40f33b4f6bd170739690f74f629fcfca288
Haishan Zhou <zhssmail@gmail.com>
2019-08-12 16:37:04 +1200

    misc: create misc folder

--- misc/setup_arch.sh
+++ misc/setup_arch.sh
@@ -3,17 +3,11 @@
 # for i3 in archlinux
 FONT_DIR=~/.local/share/fonts
 
-copy_file config/i3
-
 pacman -S sudo tmux openssh
 visudo
 sudo pacman -S i3 feh arandr ttf-font-awesome lxappearance thunar rofi compton \
-	i3blocks dialog wpa_supplicant iw ctags
+	i3blocks dialog wpa_supplicant iw ctags yay
 
 git clone https://github.com/supermarin/YosemiteSanFranciscoFont
 mkdir -p $FONT_DIR
 mv YosemiteSanFranciscoFont/*.ttf $FONT_DIR
-
-sudo cp shadowsocks.service /etc/systemd/system/shadowsocks.service
-sudo systemctl enable shadowsocks.service
-sudo systemctl enable wpa_supplicant@wlp58s0
